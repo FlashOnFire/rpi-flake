86770740dae897d7a6aca976f81287db94c3933d
FlashOnFire_ <flashonfire.dev@gmail.com>
2026-02-26 22:46:28 +0100

    fix: git cross since flake update

--- flake.nix
+++ flake.nix
@@ -20,6 +20,11 @@
       flake = false;
     };
 
+    nixpkgs-patch-fix-git-cross-compile = {
+      url = "https://github.com/FlashOnFire/nixpkgs/commit/9da532b0fb2e56c07d19fc2031ed8475ea62a208.patch";
+      flake = false;
+    };
+
     agenix = {
       url = "github:ryantm/agenix";
       inputs = {
